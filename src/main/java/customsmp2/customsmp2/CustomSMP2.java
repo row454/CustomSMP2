42e05bae58f795e8b83ecbf2b27ec4778d45ae81
row454 <rowanhope5@gmail.com>
2023-02-18 15:03:41 +0000

    initial commit

---
+++ src/main/java/customsmp2/customsmp2/CustomSMP2.java
@@ -0,0 +1,11 @@
+package customsmp2.customsmp2;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomSMP2 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/customsmp2/customsmp2/client/CustomSMP2Client.java
@@ -0,0 +1,11 @@
+package customsmp2.customsmp2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class CustomSMP2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
